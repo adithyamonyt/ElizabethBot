1925491c58630c5a27b59873bcd488756876a4d4
adithyamonyt <86489565+adithyamonyt@users.noreply.github.com>
2021-06-25 23:08:39 +0530

    Update __main__.py

--- Elizabeth/__main__.py
+++ Elizabeth/__main__.py
@@ -39,8 +39,8 @@ from Elizabeth.modules.helper_funcs.alternate import typing_action
 
 
 PM_START_TEXT = f"""
-`Hi..Welcome` [🦋](https://telegra.ph/file/debaabe44781f92783315.jpg)
-`I'm`  єlízαвєth
+`Hi..Welcome` [🦋](https://telegra.ph/file/a928d734a48889d3b9edb.jpg)
+`I'm`  Angel
 `I'm here to help you manage your groups.. Click Help button to find out more about how to use me to my full potential..`
 """
 
@@ -63,7 +63,7 @@ buttons += [[InlineKeyboardButton(text="🎀 CLOSE THE MENU 🎀",
 
 
 HELP_STRINGS = f"""
-`Hi.. I'm` єlízαвєth[🦋](https://telegra.ph/file/54ed9e0d6eef2cc730f12.jpg)
+`Hi.. I'm` Angel [🦋](https://telegra.ph/file/a928d734a48889d3b9edb.jpg)
 `Click on the buttons below to get documentation about specific modules..`"""
 
 
